ed754c3460a68a462973f98d3c7488bfccb6300a
Nagy Zétény <nagyzeteny6@gmail.com>
2026-02-04 21:17:19 +0100

    fix: move admin check to admin layout, to resolve infinite loop

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -1,22 +1,9 @@
-import { NextResponse } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-import { checkIsAdmin } from '~/server/utils/check-is-admin';
-
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
-const isAdminRoute = createRouteMatcher(['/dashboard/admin(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
-
-  if (isAdminRoute(req)) {
-    const { isAuthenticated, userId } = await auth();
-
-    if (!isAuthenticated) await auth.protect();
-
-    const isAdmin = await checkIsAdmin(userId);
-    if (!isAdmin) return NextResponse.redirect(new URL('/forbidden', req.url));
-  }
 });
 
 export const config = {
