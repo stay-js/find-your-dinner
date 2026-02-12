06ac6504249daf0a0bf0d77243a24602717b6b8b
Polyák Panna <ppanna2005@gmail.com>
2026-02-12 16:21:34 +0100

    feat: edit page get recipe, add recept form

--- web/src/app/dashboard/recipes/edit/[id]/page.tsx
+++ web/src/app/dashboard/recipes/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
+import { auth } from '@clerk/nextjs/server';
+import { notFound } from 'next/navigation';
+
+import { getRecipe } from '~/server/utils/get-recipe';
+import { idParamSchema } from '~/lib/zod-schemas';
 import { createMetadata } from '~/lib/create-metadata';
+import { RecipeForm } from '~/components/recipe-form';
 
 export const metadata = createMetadata({
   path: '/dashboard/recipes/edit',
@@ -7,10 +13,34 @@ export const metadata = createMetadata({
   noIndex: true,
 });
 
-export default async function EditPage() {
-  return (
-    <div>
-      <h1>Recept szerkesztése</h1>
-    </div>
-  );
+export default async function EditPage({ params }: { params: Promise<{ id: string }> }) {
+  const { userId } = await auth();
+
+  const result = idParamSchema.safeParse(await params);
+  if (!result.success) notFound();
+
+  const { id } = result.data;
+
+  const { recipe, recipeData, categories, ingredients, author } = await getRecipe(id, true);
+
+  if (author.id !== userId) notFound();
+
+  const defaultValues = {
+    recipeId: recipe.id,
+    title: recipeData.title,
+    previewImageUrl: recipeData.previewImageUrl,
+    description: recipeData.description,
+    instructions: recipeData.instructions,
+    prepTimeMinutes: recipeData.prepTimeMinutes.toString(),
+    cookTimeMinutes: recipeData.cookTimeMinutes.toString(),
+    servings: recipeData.servings.toString(),
+    categories: categories.map((c) => c.id),
+    ingredients: ingredients.map((i) => ({
+      ingredientId: i.ingredient.id.toString(),
+      quantity: i.quantity.toString(),
+      unitId: i.unit.id.toString(),
+    })),
+  };
+
+  return <RecipeForm defaultValues={defaultValues} />;
 }
