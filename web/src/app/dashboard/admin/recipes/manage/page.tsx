b161e2e277d55bbb86e2c8455a42f7f3b623a8f6
Polyák Panna <ppanna2005@gmail.com>
2026-02-27 09:36:23 +0100

    feat: add admin manage recipes page

--- web/src/app/dashboard/admin/recipes/manage/page.tsx
+++ web/src/app/dashboard/admin/recipes/manage/page.tsx
@@ -1,5 +1,7 @@
 import { createMetadata } from '~/lib/create-metadata';
 
+import { Recipes } from './recipes';
+
 export const metadata = createMetadata({
   path: '/dashboard/admin/recipes/manage',
 
@@ -13,6 +15,8 @@ export default async function AdminManagePage() {
   return (
     <div className="container flex flex-col gap-4">
       <h1 className="text-foreground text-2xl font-semibold">Receptek kezelése</h1>
+
+      <Recipes />
     </div>
   );
 }
---
+++ web/src/app/dashboard/admin/recipes/manage/recipes.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { NoContent } from '~/components/no-content';
+import { PaginationComponent } from '~/components/pagination-component';
+import { RecipeCard } from '~/components/recipe-card';
+import { RecipeCardSkeleton } from '~/components/recipe-card-skeleton';
+import { useSidebar } from '~/components/ui/sidebar';
+import { useCreateQueryString } from '~/hooks/use-create-query-string';
+import { GET } from '~/lib/api';
+import { cn } from '~/lib/utils';
+import { pageSchema, paginatedRecipesSchema } from '~/lib/zod';
+
+export function Recipes() {
+  const { open: isSidebarOpen } = useSidebar();
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const createQueryString = useCreateQueryString(searchParams);
+
+  const page = pageSchema.parse(searchParams.get('page'));
+
+  const { data: recipes, isLoading } = useQuery({
+    queryFn: () => GET(`/api/recipes?allow-unverified=true&page=${page}`, paginatedRecipesSchema),
+    queryKey: ['recipes', 'admin', { page }],
+  });
+
+  const currentApiPage = recipes?.meta?.currentPage;
+
+  useEffect(() => {
+    if (!currentApiPage || currentApiPage === page) return;
+
+    router.replace(pathname + '?' + createQueryString('page', currentApiPage.toString()), {
+      scroll: false,
+    });
+  }, [currentApiPage, page, pathname, router, createQueryString]);
+
+  return (
+    <>
+      {!isLoading && (!recipes || recipes.data.length === 0) && (
+        <NoContent
+          create={null}
+          description="Úgy tűnik, még nincs egyetlen recept sem."
+          title="Nincs megjeleníthető recept"
+        />
+      )}
+
+      <div
+        className={cn(
+          'grid grid-cols-1 gap-6',
+          isSidebarOpen ? 'lg:grid-cols-2 xl:grid-cols-3' : 'sm:grid-cols-2 lg:grid-cols-3',
+        )}
+      >
+        {isLoading && new Array(3).fill(null).map((_, i) => <RecipeCardSkeleton key={i} />)}
+
+        {recipes?.data.map((recipe) => (
+          <RecipeCard key={recipe.recipe.id} pageType="admin" recipe={recipe} showIsVerified />
+        ))}
+      </div>
+
+      <PaginationComponent currentPage={page} pageCount={recipes?.meta.pageCount || 1} />
+    </>
+  );
+}
