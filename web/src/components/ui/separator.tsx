e98f43cb008d152c5a01f26b264690bb63d499f3
Nagy Zétény <nagyzeteny6@gmail.com>
2026-03-20 18:47:35 +0100

    fix: separator width and height

--- web/src/components/ui/separator.tsx
+++ web/src/components/ui/separator.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Separator as SeparatorPrimitive } from 'radix-ui';
+import * as SeparatorPrimitive from '@radix-ui/react-separator';
 import * as React from 'react';
 
 import { cn } from '~/lib/utils';
@@ -14,7 +14,7 @@ function Separator({
   return (
     <SeparatorPrimitive.Root
       className={cn(
-        'bg-border shrink-0 data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch',
+        'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
         className,
       )}
       data-slot="separator"
