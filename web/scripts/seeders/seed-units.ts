662c8463ddf08121e63031877771b0a2edd0db98
Polyák Panna <ppanna2005@gmail.com>
2026-04-08 18:09:16 +0200

    feat: extend units seeder

--- web/scripts/seeders/seed-units.ts
+++ web/scripts/seeders/seed-units.ts
@@ -5,17 +5,29 @@ import { units } from '~/server/db/schema';
 
 const data = [
   { abbreviation: 'g', name: 'Gramm' },
+  { abbreviation: 'dkg', name: 'Dekagramm' },
   { abbreviation: 'kg', name: 'Kilogramm' },
+
   { abbreviation: 'ml', name: 'Milliliter' },
+  { abbreviation: 'cl', name: 'Centiliter' },
+  { abbreviation: 'dl', name: 'Deciliter' },
   { abbreviation: 'l', name: 'Liter' },
+
   { abbreviation: 'db', name: 'Darab' },
+
   { abbreviation: 'tk', name: 'Teáskanál' },
   { abbreviation: 'ek', name: 'Evőkanál' },
+
   { abbreviation: 'csipet', name: 'Csipet' },
+
   { abbreviation: 'pohár', name: 'Pohár' },
   { abbreviation: 'bögre', name: 'Bögre' },
+
   { abbreviation: 'csomag', name: 'Csomag' },
+
   { abbreviation: 'gerezd', name: 'Gerezd' },
+
+  { abbreviation: 'szelet', name: 'Szelet' },
 ] satisfies (typeof units.$inferInsert)[];
 
 export async function seedUnits() {
