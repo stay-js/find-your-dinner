3841cf283a71dff64a2dfcabcc7008dbd27f2729
Nagy Zétény <nagyzeteny6@gmail.com>
2026-04-09 06:50:54 +0200

    fix: react query provider leaking cache

--- web/src/app/react-query-provider.tsx
+++ web/src/app/react-query-provider.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      staleTime: 60,
-    },
-  },
-});
+import { useState } from 'react';
 
 export function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
