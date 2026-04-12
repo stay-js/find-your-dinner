a169b7928438bd607a1219780e0f1db3415cfc34
K. Papp Benjámin <k.papp.benjamin@gmail.com>
2026-04-12 15:21:00 +0200

    feat: add myself to admin seeder

--- web/scripts/seeders/seed-admins.ts
+++ web/scripts/seeders/seed-admins.ts
@@ -4,8 +4,10 @@ import { db } from '~/server/db';
 import { admins } from '~/server/db/schema';
 
 const data = [
-  { userId: 'user_38bzMM6AVsxkk7dJNc7n4GLSkDm' }, // znagy, dev
-  { userId: 'user_3CCx7CfdpF5IWcdjVb0DmpUppw5' }, // ppanna, dev
+  // clerk dev environment
+  { userId: 'user_38bzMM6AVsxkk7dJNc7n4GLSkDm' }, // znagy
+  { userId: 'user_3CCx7CfdpF5IWcdjVb0DmpUppw5' }, // ppanna
+  { userId: 'user_3CG6nCYzPeHOgYIZg6oajghAz5e' }, // zsepy
 ] satisfies (typeof admins.$inferInsert)[];
 
 export async function seedAdmins() {
