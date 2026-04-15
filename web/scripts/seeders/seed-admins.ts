0c5c24fc6947760edf1dd7327ed72755a16fadd4
Nagy Zétény <nagyzeteny6@gmail.com>
2026-04-15 20:32:41 +0200

    feat: add my prod clerk user id to admin seeder

--- web/scripts/seeders/seed-admins.ts
+++ web/scripts/seeders/seed-admins.ts
@@ -8,6 +8,9 @@ const data = [
   { userId: 'user_38bzMM6AVsxkk7dJNc7n4GLSkDm' }, // znagy
   { userId: 'user_3CCx7CfdpF5IWcdjVb0DmpUppw5' }, // ppanna
   { userId: 'user_3CG6nCYzPeHOgYIZg6oajghAz5e' }, // zsepy
+
+  // clerk prod environment
+  { userId: 'user_3CPCdO9xP7lG2xuj7xsd466sLdd' }, // znagy
 ] satisfies (typeof admins.$inferInsert)[];
 
 export async function seedAdmins() {
