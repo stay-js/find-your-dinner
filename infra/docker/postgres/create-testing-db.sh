563dbe762cbc0164f3d44c061be9602b705ca4e1
Nagy Zétény <nagyzeteny6@gmail.com>
2026-04-20 21:48:24 +0200

    feat: create testing db

---
+++ infra/docker/postgres/create-testing-db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "testing";
+    GRANT ALL PRIVILEGES ON DATABASE "testing" TO "$POSTGRES_USER";
+EOSQL
