5bc61290add0240cfee5050d1ea22dac733b6d31
Nagy Zétény <nagyzeteny6@gmail.com>
2026-04-23 20:48:46 +0200

    feat: add dev env userIds to default ingredient seeder

--- web/scripts/seeders/seed-default-ingredients.ts
+++ web/scripts/seeders/seed-default-ingredients.ts
@@ -3,46 +3,44 @@ import 'dotenv/config';
 import { db } from '~/server/db';
 import { defaultIngredients, ingredients } from '~/server/db/schema';
 
-type DefaultIngredientData = {
-  userId: string;
+const userIdsToSeedWithSelectedIngedients = [
+  'user_38bzMM6AVsxkk7dJNc7n4GLSkDm', // znagy clerk dev environment
+  'user_3CPCdO9xP7lG2xuj7xsd466sLdd', // znagy clerk prod environment
+];
 
-  ingredientNames: string[];
-}[];
+const userIdsToSeedWithAllIngredients = [
+  'user_3Clo0wtsXPon7mNlAazTciKb0Lv', // znagy 2 clerk dev environment
+  'user_3CkzswbYbPXoT2B5OufMVDWwCOK', // znagy 2 clerk prod environment
+];
 
-const data = [
-  {
-    userId: 'user_3CPCdO9xP7lG2xuj7xsd466sLdd', // znagy clerk prod environment
-
-    ingredientNames: [
-      'Babérlevél',
-      'Balzsamecet',
-      'Bazsalikom',
-      'Búzaliszt',
-      'Cukor',
-      'Ecet',
-      'Fahéj',
-      'Fekete bors',
-      'Kakaópor',
-      'Kakukkfű',
-      'Ketchup',
-      'Kínai ötfűszer',
-      'Kömény',
-      'Majonéz',
-      'Mustár',
-      'Napraforgóolaj',
-      'Olívaolaj',
-      'Őrölt koriander',
-      'Őrölt paprika',
-      'Porcukor',
-      'Rozmaring',
-      'Só',
-      'Sütőpor',
-      'Szegfűszeg',
-      'Szódabikarbóna',
-      'Víz',
-    ],
-  },
-] satisfies DefaultIngredientData;
+const defaultIngredientNames = [
+  'Babérlevél',
+  'Balzsamecet',
+  'Bazsalikom',
+  'Búzaliszt',
+  'Cukor',
+  'Ecet',
+  'Fahéj',
+  'Fekete bors',
+  'Kakaópor',
+  'Kakukkfű',
+  'Ketchup',
+  'Kínai ötfűszer',
+  'Kömény',
+  'Majonéz',
+  'Mustár',
+  'Napraforgóolaj',
+  'Olívaolaj',
+  'Őrölt koriander',
+  'Őrölt paprika',
+  'Porcukor',
+  'Rozmaring',
+  'Só',
+  'Sütőpor',
+  'Szegfűszeg',
+  'Szódabikarbóna',
+  'Víz',
+];
 
 export async function seedDefaultIngredients() {
   console.log('⏳ Seeding default ingredients...');
@@ -50,29 +48,22 @@ export async function seedDefaultIngredients() {
   const allIngredients = await db.select().from(ingredients);
   const ingredientByName = Object.fromEntries(allIngredients.map(({ id, name }) => [name, id]));
 
-  await db
-    .insert(defaultIngredients)
-    .values(
-      allIngredients.map(({ id }) => ({
-        ingredientId: id,
-        userId: 'user_3CkzswbYbPXoT2B5OufMVDWwCOK', // znagy 2 clerk prod environment
-      })),
-    )
-    .onConflictDoNothing();
+  for (const userId of userIdsToSeedWithAllIngredients) {
+    await db
+      .insert(defaultIngredients)
+      .values(allIngredients.map(({ id }) => ({ ingredientId: id, userId })))
+      .onConflictDoNothing();
+  }
 
-  for (const item of data) {
+  for (const userId of userIdsToSeedWithSelectedIngedients) {
     await db
       .insert(defaultIngredients)
       .values(
-        item.ingredientNames.map((name) => {
+        defaultIngredientNames.map((name) => {
           const ingredientId = ingredientByName[name];
           if (!ingredientId) throw new Error(`Ingredient not found: "${name}"`);
 
-          return {
-            userId: item.userId,
-
-            ingredientId,
-          };
+          return { ingredientId, userId };
         }),
       )
       .onConflictDoNothing();
